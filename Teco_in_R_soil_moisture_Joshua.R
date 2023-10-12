eb3dcd5b8ecb0909afa030523ad73ea6a1e4ae44
Joshuaajowele <joshuaajowele@gmail.com>
2023-10-12 06:42:29 -0400

    Run off and water redistribution

--- Teco_in_R_soil_moisture_Joshua.R
+++ Teco_in_R_soil_moisture_Joshua.R
@@ -6,6 +6,7 @@ Precip<-5
 #Precip<-runif(10,1,10)
 Infilt<-Precip
 FieldCap<-0.45
+WILTPT<-0
 Thick<-100
 Wc<-0
 Wtdeficit<-FieldCap-Wc
@@ -22,8 +23,22 @@ roff_layer<-0
 if(Perc >0) {roff_layer=roff_layer + Perc*0.05}
 Infilt_new<-Perc-Perc*0.05
 
+#runoff
+runoff=Infilt_new + roff_layer  #weng 10072006
 
-#if(precp.gt.0.0.and.wcl(1).gt.wcl(2))then
-supply=(wcl(1)-wcl(2))/3.0
-wcl(1)=wcl(1)-2.0*supply
-wcl(2)=wcl(2)+supply
+##	water redistribution among soil layers
+wsc<-max(0.00,(Wcnew-WILTPT)*Thick*10.0)
+omegaL=max(0.001,(Wcnew-WILTPT)/(FieldCap-WILTPT))
+
+
+supply=0.0
+demand=0.0
+
+if(omegaL >0.3){supply=wsc/360.0*omegaL
+demand<-(FieldCap-Wcnew*Thick*10.0/360.0*(1.0-omegaL))
+exchangeL=min(supply,demand)
+wsc_min=wsc-exchangeL
+wsc_max=wsc+exchangeL
+Wcnew_min=wsc_min/(Thick*10.0)+WILTPT
+Wcnew_max=wsc_max/(Thick*10.0)+WILTPT
+}
